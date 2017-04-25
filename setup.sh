602126a4d5d431962b1b7adfc1cea13be46dd11f
Álvaro Brey <alvaro.brv@gmail.com>
2017-04-25 12:03:38 +0200

    setup.sh: fix typo

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # do symlink
-sudo cp -f "$(pwd)/51-android,rules" /etc/udev/rules.d/
+sudo cp -f "$(pwd)/51-android.rules" /etc/udev/rules.d/
 sudo chmod a+r /etc/udev/rules.d/51-android.rules
 # restart udev
 sudo udevadm control --reload-rules
