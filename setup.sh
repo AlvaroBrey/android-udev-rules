8ee4ea75fefa46fe58a1da5c40638fb9b82ad1cd
Álvaro Brey <alvaro.brv@gmail.com>
2017-04-25 12:24:38 +0200

    Refactor: put vendor IDS in a plaintext file and generate rules

--- setup.sh
+++ setup.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
+FILENAME="51-android.rules"
+
+function genRules() {
+    echo "# udev rules for android devices (adb/fastboot)" > $FILENAME
+    echo "# This file was automatically generated" >> $FILENAME
+    echo >> $FILENAME
+    local vendors=$(cut -f 1 -d ' ' < vendor-ids.txt)
+    for vendor in $vendors
+    do
+      echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"$vendor\", MODE=\"0666\", GROUP=\"plugdev\"" >> $FILENAME
+    done
+}
+
+# generate rules file
+genRules
 # do symlink
-sudo cp -f "$(pwd)/51-android.rules" /etc/udev/rules.d/
-sudo chmod a+r /etc/udev/rules.d/51-android.rules
+sudo cp -f "$(pwd)/$FILENAME" /etc/udev/rules.d/
+sudo chmod a+r "/etc/udev/rules.d/$FILENAME"
 # restart udev
 sudo udevadm control --reload-rules
 sudo service udev restart
