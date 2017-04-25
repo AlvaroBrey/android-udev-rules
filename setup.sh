d2769f663d17ade12cfb8ae4374b79abd8d9f3e5
Álvaro Brey <alvaro.brv@gmail.com>
2017-04-25 12:01:10 +0200

    setup: copy instead of linking

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # do symlink
-sudo ln -sf "$(pwd)/51-android,rules" /etc/udev/rules.d/
+sudo cp -f "$(pwd)/51-android,rules" /etc/udev/rules.d/
+sudo chmod a+r /etc/udev/rules.d/51-android.rules
 # restart udev
 sudo udevadm control --reload-rules
 sudo service udev restart
